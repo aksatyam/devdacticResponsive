447e0c857be00f601b689f548aacaa054677e6a3
aksatyam <ashishsatyam.kmr@gmail.com>
2018-04-27 12:55:24 +0530

    Changes done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Platform,AlertController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import "rxjs/add/operator/map";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+ selector: 'page-home',
+ templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+ users: Observable<any>;
 
-  }
-
+ constructor(public navCtrl: NavController, private httpClient: HttpClient, private plt: Platform, private alertCtrl: AlertController) {
+   this.users = this.httpClient.get('https://randomuser.me/api/?results=20')
+   .map(res => res['results'])
+ }
 }
